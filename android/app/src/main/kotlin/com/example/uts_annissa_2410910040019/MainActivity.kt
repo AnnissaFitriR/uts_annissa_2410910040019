f110b0e8905f0496acddd3e63e891950249f4c22
Annissafr <annissafitriramadhani9@gmail.com>
2025-10-23 19:29:17 +0700

    utsAnnissa1

---
+++ android/app/src/main/kotlin/com/example/uts_annissa_2410910040019/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.uts_annissa_2410910040019
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/ujian/uts_annissa_2410910040019.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class UtsAnnissa2410910040019 extends StatelessWidget {
+  const UtsAnnissa2410910040019({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
