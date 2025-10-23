ca954d2aa55a8982a369798dd534a45143cd82a4
Annissafr <annissafitriramadhani9@gmail.com>
2025-10-23 19:55:55 +0700

    push1

--- lib/ujian/uts_annissa_2410910040019.dart
+++ lib/ujian/uts_annissa_2410910040019.dart
@@ -1,10 +1,28 @@
 import 'package:flutter/material.dart';
 
-class ut extends StatelessWidget {
-  const ut({super.key});
+class UtsAnnissa2410910040019 extends StatelessWidget {
+  const UtsAnnissa2410910040019({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Image.asset("gambar/Logo.png"),
+          Column(
+            children: [
+              Text("LKS",
+              style: TextStyle(fontSize: 60, fontWeight: FontWeight.bold, color: Colors.blueGrey),
+              ),
+              Text("MART",
+              style: TextStyle(fontSize: 60, fontWeight: FontWeight.bold, color: Colors.blueGrey),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
   }
 }
