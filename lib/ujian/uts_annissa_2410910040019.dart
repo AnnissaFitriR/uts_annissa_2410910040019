61ff1c3a2e808a990e9c8c2ca3e2f3b21209e4ab
Annissafr <annissafitriramadhani9@gmail.com>
2025-10-23 19:35:11 +0700

    uts_annissaa

--- lib/ujian/uts_annissa_2410910040019.dart
+++ lib/ujian/uts_annissa_2410910040019.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
-class UtsAnnissa2410910040019 extends StatelessWidget {
-  const UtsAnnissa2410910040019({super.key});
+class ut extends StatelessWidget {
+  const ut({super.key});
 
   @override
   Widget build(BuildContext context) {
