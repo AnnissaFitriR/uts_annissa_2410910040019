4e8bb8607468018fdedfc5735ac09baeb4c6dfc5
Annissafr <annissafitriramadhani9@gmail.com>
2025-10-23 21:15:31 +0700

    push1

--- lib/ujian/uts_annissa_2410910040019.dart
+++ lib/ujian/uts_annissa_2410910040019.dart
@@ -1,28 +1,162 @@
 import 'package:flutter/material.dart';
 
-class UtsAnnissa2410910040019 extends StatelessWidget {
+class UtsAnnissa2410910040019 extends StatefulWidget {
   const UtsAnnissa2410910040019({super.key});
 
   @override
+  State<UtsAnnissa2410910040019> createState() => _UtsAnnissa2410910040019State();
+}
+
+class _UtsAnnissa2410910040019State extends State<UtsAnnissa2410910040019> {
+  bool _showpassword = true;
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Row(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Image.asset("gambar/Logo.png"),
-          Column(
+      body: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 24, vertical: 30),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              Text("LKS",
-              style: TextStyle(fontSize: 60, fontWeight: FontWeight.bold, color: Colors.blueGrey),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Image.asset(
+                    "gambar/Logo.png",
+                    width: 80,
+                    height: 80,
+                  ),
+                  const SizedBox(width: 12),
+                  const Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'LKS',
+                        style: TextStyle(
+                          fontSize: 42,
+                          fontWeight: FontWeight.bold,
+                          color: Color.fromARGB(255, 79, 91, 99),
+                        ),
+                      ),
+                      Text(
+                        'MART',
+                        style: TextStyle(
+                          fontSize: 42,
+                          fontWeight: FontWeight.bold,
+                          color: Color.fromARGB(255, 79, 91, 99),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
               ),
-              Text("MART",
-              style: TextStyle(fontSize: 60, fontWeight: FontWeight.bold, color: Colors.blueGrey),
+              SizedBox(height: 40),
+              Text(
+                'Sign In',
+                style: TextStyle(
+                  fontSize: 22,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(height: 8),
+              Text(
+                'Enter your ID and password to sign in!',
+                style: TextStyle(
+                  fontSize: 14,
+                  color: Colors.grey,
+                ),
+              ),
+              SizedBox(height: 30),
+              Text(
+                'Email',
+                style: TextStyle(fontWeight: FontWeight.normal),
+              ),
+              SizedBox(height: 8),
+              TextField(
+                decoration: InputDecoration(
+                  hintText: 'email',
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  contentPadding: EdgeInsets.symmetric(horizontal: 12, vertical: 14),
+                ),
+              ),
+              SizedBox(height: 20),
+              Text(
+                'Password*',
+                style: TextStyle(fontWeight: FontWeight.w500),
+              ),
+              SizedBox(height: 8),
+              TextField(
+                obscureText: true,
+                decoration: InputDecoration(
+                  hintText: 'Min. 8 characters',
+                  suffixIcon: Icon(
+                    _showpassword
+                        ? Icons.remove_red_eye
+                        : Icons.visibility_off,
+                  ),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  contentPadding: EdgeInsets.symmetric(horizontal: 12, vertical: 14),
+                ),
+              ),
+            SizedBox(height: 10),
+              Row(
+                children: [
+                  Checkbox(
+                    value: false,
+                    onChanged: (val) {},
+                    activeColor: Colors.blueGrey,
+                  ),
+                  Text(
+                    'Keep me logged in',
+                    style: TextStyle(color: Colors.black),
+                  ),
+                ],
+              ),
+              SizedBox(height: 10),
+              SizedBox(
+                width: double.infinity,
+                height: 50,
+                child: ElevatedButton(
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor:  Color.fromARGB(255, 62, 90, 128),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                  ),
+                  child: Text(
+                    'Login',
+                    style: TextStyle(
+                      fontSize: 18,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(height: 20),
+              Center(
+                child: RichText(
+                  text: TextSpan(
+                    text: 'Belum punya akun? ',
+                    style: TextStyle(color: Colors.black),
+                    children: [
+                      TextSpan(
+                        text: 'Daftar di sini',
+                        style: TextStyle(
+                          color: Colors.blue,
+                          fontWeight: FontWeight.w600,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
               ),
             ],
           ),
-        ],
-      ),
+        ),
     );
   }
 }
